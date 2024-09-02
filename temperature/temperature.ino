6357b369c105ebb55ef17f7ad0966135193be2ae
Nikola Vujic <nikvujic@gmail.com>
2024-09-02 10:51:54 +0200

    Temperature sensor code added

--- temperature/temperature.ino
+++ temperature/temperature.ino
@@ -1,5 +1,6 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <DHT.h>  // Make sure to import library for dht sensors
 
 // Add your network credentials
 const char* ssid = "***";
@@ -11,36 +12,46 @@ const char* serverName = "https://***/api/device-events/log";
 // Set your license key
 const char* licenseKey = "***";
 
-int counter = 0;
+#define DHTPIN 4          // DHT sensor data pin
+#define DHTTYPE DHT11     // DHT11 or DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(115200);
 
+  dht.begin();
+
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
-  
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  
+
   Serial.println("\nConnected to WiFi");
 }
 
 void loop() {
-  counter = counter + 1;
-  
+  float temperature = dht.readTemperature();
+
+  if (isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
   if(WiFi.status()== WL_CONNECTED){
 
     HTTPClient http;
 
     http.begin(serverName);
-    
+
     http.addHeader("Content-Type", "application/json");
-    
-    String jsonData = "{\"license\": \"" + String(licenseKey) + "\", \"data\": \"{\\\"count\\\":\\\"" + String(counter) + "\\\", \\\"message\\\":\\\"Counter increased to " + String(counter) + "\\\"}\"}";
+
+    String jsonData = "{\"license\": \"" + String(licenseKey) + "\", \"data\": \"{\\\"temperature\\\":\\\"" + String(temperature) + "\\\", \\\"message\\\":\\\"Temperature reading: " + String(temperature) + "°C\\\"}\"}";
     int httpResponseCode = http.POST(jsonData);
-    
+
     if (httpResponseCode > 0) {
       String response = http.getString();
       Serial.println(httpResponseCode);
@@ -56,6 +67,6 @@ void loop() {
   else {
     Serial.println("Error in WiFi connection");
   }
-  
-  delay(5000);
+
+  delay(15000);
 }
